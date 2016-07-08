41b5d4aff062a6d2460548a36436c22d99a9f3fa
fermisfit <you@example.com>
2016-07-08 12:28:19 -0500

    elegir simbolo

---
+++ features/elegirSimbolo.feature
@@ -0,0 +1,20 @@
+#language: es
+
+Característica: un jugador puede elegir el simbolo, X o O
+
+Escenario: un jugador ve las opciones para elegir un simbolo X o O
+  Dado estoy eligiendo simbolo
+  Entonces debo ver "Jugador 1"
+  Y debo ver "X"
+
+Escenario: un jugador ve las opciones para elegir un simbolo X o O
+  Dado estoy eligiendo simbolo
+  Entonces debo ver "Jugador 2"
+  Y debo ver "O"
+
+
+Escenario: un jugador elije el simbolo X
+  Dado estoy eligiendo simbolo
+  Cuando el jugador elije el simbolo "simboloX"
+  Y inicio un juego
+  Entonces debo ver "Jugador 1 jugará con X"
--- features/step_definitions/my_steps.rb
+++ features/step_definitions/my_steps.rb
@@ -1,6 +1,11 @@
 Given(/^abri el juego$/) do
+  visit '/juego', :post
+end
+
+Given(/^estoy eligiendo simbolo$/) do
   visit '/'
 end
+
 
 Then(/^debo ver el titulo "(.*?)"$/) do |tituloJuego|
   last_response.body.should =~ /#{tituloJuego}/m
@@ -12,4 +17,17 @@ end
 
 Then(/^el tablero debe tener "(.*?)" casillas$/) do |numeroImagenes|
   last_response.should have_xpath("//img[@id=\"imagen#{numeroImagenes}\"]")
+end
+
+Entonces(/^debo ver "(.*?)"$/) do |simbolo|
+  last_response.body.should =~ /#{simbolo}/m
+end
+
+
+Cuando(/^el jugador elije el simbolo "(.*?)"$/) do |simbolo|
+  choose(simbolo)
+end
+
+Cuando(/^inicio un juego$/) do
+  click_button("elegir")
 end
