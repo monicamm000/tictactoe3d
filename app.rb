d32ae0ba64f68885ecc1525eb834409e1f724804
kleer <monica.medellin@outlook.com>
2016-07-08 15:37:42 -0500

    llenar casillas segun turno

--- app.rb
+++ app.rb
@@ -13,14 +13,21 @@ post '/juego' do
   session["simboloObj"] = Simbolo.new params["simbolo"]
   session["simboloJ2"] = session["simboloObj"].getSimbolo
   session["turno"] = "Jugador 1"
+  session["casillas"] = ["1","2","3","4","5","6","7","8","9"]
   erb :index
 end
 
 post '/marcar' do
   if session["turno"] == "Jugador 1"
     session["turno"] = "Jugador 2"
+    simboloEnTurno = session["simboloJ1"]
   else
     session["turno"] = "Jugador 1"
+    simboloEnTurno = session["simboloJ2"]
   end
+  @casilla = params["numCasilla"]
+  session["casillas"][@casilla.to_i - 1] = simboloEnTurno
+
+
     erb :index
 end
